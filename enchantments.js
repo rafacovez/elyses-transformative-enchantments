7e76962c4e76c0e5a9cfc04a164297c53b03dc2e
Adán Estévez <adanescollante@gmail.com>
2023-03-22 10:23:38 -0300

    coded middleTwo function

--- enchantments.js
+++ enchantments.js
@@ -41,7 +41,7 @@ export function threeOfEachThree(deck) {
  * @returns {number[]} deck with only two middle cards
  */
 export function middleTwo(deck) {
-  throw new Error('Implement the middleTwo function');
+  return deck.slice(4, 6);
 }
 
 /**
